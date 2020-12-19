3f6293d3dcb344a8826f2643b5dc10419c289d68
webra <li_zhm@qq.com>
2020-12-19 20:58:44 +0800

    更新程序入口名称

--- blog-admin/src/main/resources/static/js/nav-tail.js
+++
@@ -1,7 +0,0 @@
-function heightRsize(){
-    if($(window).height() > height){
-        $(".footer").css({"position":"absolute","width":"100%","bottom":"0"})
-    }else{
-        $(".footer").removeAttr("style");
-    }
-}
